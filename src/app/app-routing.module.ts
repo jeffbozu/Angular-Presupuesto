fd3c9ea12537953a6ab7732834f9040196a1f73e
jeffrey <j.e.bolanos@hotmail.com>
2022-11-01 23:45:25 +0100

    parte1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes//
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/ingresarPresupuesto', pathMatch:'full'}, /*si esta vacio '', nos redirige a ingresar presupesto*/
+  { path: 'ingresarPresupuesto', component: IngresarPresupuestoComponent },
+  { path: 'gastos', component: GastosComponent },
+  {path:'**', redirectTo: '/ingresarPresupuesto', pathMatch: 'full'}, /* es importante que este de ultimo, si escribimos mal '**'nos redirige a ingresarpresupuesto*/
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
